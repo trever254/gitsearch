4d47b4094a720656991ae2d71b25e1752133cc5f
trever <toakumu@gmail.com>
2020-06-22 09:47:33 +0300

    added component file

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GitService } from '../git/git.service';
 @Component({
   selector: 'app-repo',
   templateUrl: './repo.component.html',
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  users: any = [];
+  repos: any = [];
+  username: string;
 
-  constructor() { }
+  constructor(Private gitService: GitService) { }
 
   ngOnInit(): void {
+    this.gitService.gitProfile()
+      .subscribe(res => {
+        //console.log(res)
+        this.users = res;
+      })
+
+    this.gitService.gitRepos()
+      .subscribe(data => {
+        //console.log(data)
+        this.repos = data;
+      })
   }
+  searchUser() {
+    this.gitService.updateUser(this.username);
+
+    this.gitService.gitProfile()
+      .subscribe(res => {
+        //console.log(res)
+        this.users = res;
+      })
+
+    this.gitService.gitRepos()
+      .subscribe(data => {
+        //console.log(data)
+        this.repos = data;
+      })
+  }
+
 
 }
