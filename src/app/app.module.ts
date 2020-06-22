0f17b6cac9d1e32420b20268569a6f7201cec45e
trever <toakumu@gmail.com>
2020-06-22 05:48:03 +0300

    added changes in th app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import{NgProgressHttpClientModule} from '@ngx-progressbar/http-client';
+
 import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+import { GitService } from './git/git.service';
+import { RepoComponent } from './repo/repo.component';
+import { AppRoutingModule} from './app-routing.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+// const routes:Routes = [
+//   {path:"git",component:GitComponent},
+//   {path:"repo",component:RepoComponent},
+// ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent,
+    RepoComponent,
+    NavBarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
-  providers: [],
+  providers: [GitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
