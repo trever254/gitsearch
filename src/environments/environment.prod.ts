5ddef1c341b8c154efe04768f7894930776dda21
trever <toakumu@gmail.com>
2020-06-23 19:31:08 +0300

    added chnges in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl:"https://api.github.com/users/",
-  accessToken: "b6eb1d434ea78917042749f45f8b6564095bd84c"
+  accessToken: "b6eb1d434ea78917042749f45f8b6564095bd84c",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"https://api.github.com/users/",
+  accessToken: "b6eb1d434ea78917042749f45f8b6564095bd84c"
 };
 
 /*
